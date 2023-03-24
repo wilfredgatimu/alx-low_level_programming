26ea4fb9a204308ccb8c96c4de7ad74675b2588a
wilfredgatimu <wilfredgatimu3@gmail.com>
2023-03-24 04:25:30 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,6 @@
 /**
  * print_line - draws a straight line in the terminal
  * @n: n is the number of times the character _ should be printed
- * Return: Straight line
  */
 void print_line(int n)
 {
