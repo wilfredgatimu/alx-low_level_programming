c16bef34183cb90efbe64c001a71b0ccdb997df0
wilfredgatimu <wilfredgatimu3@gmail.com>
2023-04-02 12:38:45 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,9 +4,11 @@
  * @n: integer params 
  * Return: 0
  */
+
 void print_number(int n)
 {
 	unsigned int n1;
+
 	n1 = n;
 
 	if (n < 0)
@@ -14,11 +16,12 @@ void print_number(int n)
 		_putchar('-');
 		n1 = -n;
 	}
+
 	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
 	}
-	_putchar((n1 % 10) * '0');
+	_putchar((n1 % 10) + '0');
 }
 
 	
