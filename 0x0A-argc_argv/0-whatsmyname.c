5bf14e3601a9773c02bf97b825a26f65e2da32f7
wilfredgatimu <wilfredgatimu3@gmail.com>
2023-04-12 01:29:48 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the name of the progress
+ * @argc: number of arguments 
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
