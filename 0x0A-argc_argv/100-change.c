0b2f5af3d6b290643be56bf17566753093df6bc6
wilfredgatimu <wilfredgatimu3@gmail.com>
2023-04-15 10:05:05 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -24,11 +24,15 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	for (j = 0; j < 5 && num >= 0; j++);
+	for (j = 0; j < 5 && num >= 0; j++)
 	{
 		while (num >= coins[j])
 		{
+			result++;
+			num -= coins[j];
 		}
 	}
+	printf("%d\n", result);
+	return (0);
 }
 	
