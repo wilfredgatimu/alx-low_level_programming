49280a047f27b937cc02b66a046544654b90e347
wilfredgatimu <wilfredgatimu3@gmail.com>
2023-04-30 11:33:45 -0700

    0-sum_them_all.c

---
+++ 0x0E-structures_typedef/0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - it adds all the parameters
+ * @n: the fixed parameters
+ * @...: an ellipse
+ * Return: the sum of all its parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum;
+
+	sum = 0;
+	va_start(ap, n);
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+	va_end(ap);
+	return (sum);
+}
